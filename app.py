1e41ffe16821dcf3da992f58e016b5732fe56376
timofey69976 <timofey69976@gmail.com>
2026-01-04 17:29:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ TOKEN = os.getenv('TOKEN')
 RENDER_URL = os.getenv('RENDER_URL', '')
 
 if not TOKEN:
-    TOKEN = '7954650918:AAFZlRTRxZEUXNq_IYACCn60WIq8y2NBSdI'
+    TOKEN = '7954650918:AAHF3GJRZKbp3ihoaWN6UeYAsxHRY-A4V3w'
 
 TUTOR_ID = 1339816111
 SUBJECTS = ["Математика", "Физика", "Химия"]
